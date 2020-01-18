369001fc1a51b397dfbbbf1db94aa162141a01b8
weichiehlo <50180038+weichiehlo@users.noreply.github.com>
2020-01-17 17:34:19 -0800

    Updates

---
+++ precache-manifest.68bf9f317859aed9d52212f948323cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d6588d97aa0a37f0d5a4ab466a739a",
+    "url": "/forkify-react/index.html"
+  },
+  {
+    "revision": "51ff4887b9debaa63fa1",
+    "url": "/forkify-react/static/css/main.f6669b2e.chunk.css"
+  },
+  {
+    "revision": "ecf1e8806cc21d2266aa",
+    "url": "/forkify-react/static/js/2.670da231.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/forkify-react/static/js/2.670da231.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51ff4887b9debaa63fa1",
+    "url": "/forkify-react/static/js/main.e1d97eed.chunk.js"
+  },
+  {
+    "revision": "88ad66bf8401e557be03",
+    "url": "/forkify-react/static/js/runtime-main.b3e45ee3.js"
+  }
+]);
